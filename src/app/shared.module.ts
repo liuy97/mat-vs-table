70524d80d5319c255d7a04832d4120c0413affe7
Yong Liu <yong.liu@gintel.com>
2020-07-07 22:23:52 +0200

    update demo

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 
 import { MatVsTableModule } from 'mat-vs-table';
-import { MatTableModule, MatSortModule, MatCheckboxModule } from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CommonModule } from '@angular/common';
 import { TableDemoComponent } from './table/table-demo';
 
--- src/app/table/table-demo.ts
+++ src/app/table/table-demo.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angular/core';
-import { MatSort, MatTableDataSource } from '@angular/material';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
 import { NAMES } from './names';
 import { COLORS } from './colors';
